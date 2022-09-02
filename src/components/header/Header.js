63b0d6e847d9f9dcba637c4d7325172765c6e818
Humza-A <humzaaamir61@gmail.com>
2022-09-02 16:48:15 -0400

    Added: static content components for catagories

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -13,66 +13,69 @@ import "./header.css";
 export default function Header(props) {
   return (
     <>
-      <Navbar bg="dark" variant="dark" className="navBar">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="navBar">
         <Container>
-          <Navbar.Brand href="#home" className="logo">
-            <img
-              alt=""
-              src="/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{" "}
-            UnderStock
-          </Navbar.Brand>
-          <Container fill>
-            <Form className="d-flex gap-2">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="justify-content-center"
-                aria-label="Search"
-              />
-              <Button className="searchBtn">Search</Button>
-            </Form>
-          </Container>
-          <Nav className="ms-auto">
-            <Container className="d-flex align-items-center userPic">
-              <svg
-                id="i-user"
-                className="user"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 32 32"
-                width="22"
-                height="22"
-                fill="none"
-                stroke="white"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2.1"
-              >
-                <path d="M22 11 C22 16 19 20 16 20 13 20 10 16 10 11 10 6 12 3 16 3 20 3 22 6 22 11 Z M4 30 L28 30 C28 21 22 20 16 20 10 20 4 21 4 30 Z" />
-              </svg>
+            <Navbar.Brand href="#home" className="logo">
+              <img
+                alt=""
+                src="/logo.svg"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{" "}
+              UnderStock
+            </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Container fill>
+              <Form className="d-flex gap-2">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="justify-content-center"
+                  aria-label="Search"
+                />
+                <Button className="searchBtn">Search</Button>
+              </Form>
             </Container>
-            <Container className="accountLabel">
-              <Nav.Link href="">My Account</Nav.Link>
-            </Container>
-            <Container className="d-flex align-items-center cartPic">
-              <svg
-                className="bi bi-cart"
-                xmlns="http://www.w3.org/2000/svg"
-                width="23"
-                height="23"
-                fill="white"
-                viewBox="0 0 16 16"
-              >
-                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-              </svg>
-            </Container>
-            <Container className="cartLabel">
-              <Nav.Link>Your Cart</Nav.Link>
-            </Container>
-          </Nav>
+            <Nav className="ms-auto">
+              <Container className="d-flex align-items-center userPic">
+                <svg
+                  id="i-user"
+                  className="user"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 32 32"
+                  width="22"
+                  height="22"
+                  fill="none"
+                  stroke="white"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2.1"
+                >
+                  <path d="M22 11 C22 16 19 20 16 20 13 20 10 16 10 11 10 6 12 3 16 3 20 3 22 6 22 11 Z M4 30 L28 30 C28 21 22 20 16 20 10 20 4 21 4 30 Z" />
+                </svg>
+              </Container>
+              <Container className="accountLabel">
+                <Nav.Link href="">My Account</Nav.Link>
+              </Container>
+              <Container className="d-flex align-items-center cartPic">
+                <svg
+                  className="bi bi-cart"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="23"
+                  height="23"
+                  fill="white"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                </svg>
+              </Container>
+              <Container className="cartLabel">
+                <Nav.Link>Your Cart</Nav.Link>
+              </Container>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
 
@@ -100,9 +103,7 @@ export default function Header(props) {
             <a class="nav-link" href="#">
               Sports & Outdoors
             </a>
-            <a class="nav-link">
-              Toys & Games
-            </a>
+            <a class="nav-link">Toys & Games</a>
           </nav>
         </Container>
       </Navbar>
