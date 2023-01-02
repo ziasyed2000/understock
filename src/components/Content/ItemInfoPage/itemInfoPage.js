8075fa1700dc3cd6838a9605389351bc0b51b994
Humza-A <humzaaamir61@gmail.com>
2023-01-02 15:24:35 -0500

    Completed: Buying Options UI Working on : ItemInfoPage

--- src/components/Content/ItemInfoPage/itemInfoPage.js
+++ src/components/Content/ItemInfoPage/itemInfoPage.js
@@ -1,4 +1,11 @@
-import { Container, Row, Col } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Dropdown,
+  DropdownButton,
+  Button,
+} from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import Header from "../../header/Header";
 import styles from "./itemInfoPage.module.css";
@@ -15,23 +22,68 @@ export default function ItemInfoPage() {
       </Container>
       <Container fluid className={styles.productPlacementContainer}>
         <Row>
-          <Col xs={5} className={styles.ppC}>
+          <Col xs={4} className={styles.ppC}>
             1 of 3
           </Col>
-          <Col xs={4} className={styles.ppC}>
-            2 of 3
+          <Col xs={5} className={styles.ppC}>
+            <Row className={styles.itemName}>
+              <h4>
+                TicWatch Pro 3 Ultra GPS Smartwatch Qualcomm SDW4100 and Mobvoi
+                Dual Processor System Wear OS Smart Watch for Men Blood Oxygen
+                IHB AFiB Detection Fatigue Assessment 3-45 Days Battery NFC Mic
+                Speaker
+              </h4>
+            </Row>
+            <Row>
+              <Row className={`${styles.BORow} ${styles.Price}`}>
+                <div>$ </div>2400 <div> 76</div>
+              </Row>
+            </Row>
+            <Row></Row>
+            <Row></Row>
           </Col>
           <Col className={`${styles.ppC} ${styles.buyingOptions}`}>
             <Container fluid>
               <Card.Body className={styles.BOInnerDiv}>
                 <Row className={`${styles.BORow} ${styles.Price}`}>
-                <div>$ </div>2400 <div> 76</div> 
+                  <div>$ </div>2400 <div> 76</div>
+                </Row>
+                <Row className={styles.BORow}>
+                  FREE delivery Friday, January 6. Order within 13 hrs 6 mins.
+                  Details
                 </Row>
                 <Row className={styles.BORow}>Set delivery Method</Row>
-                <Row className={styles.BORow}>Quantity</Row>
-                <Row className={styles.BORow}>Add to Cart</Row>
-                <Row className={styles.BORow}>Buy Now</Row>
-                <Row className={styles.BORow}>Return Policy</Row>
+                <Row className={`${styles.BORow} ${styles.Quantity}`}>
+                  <div>Quantity:</div>
+                  <DropdownButton
+                    id="nav-dropdown-dark-example"
+                    variant="dark"
+                    title="Quantity"
+                  >
+                    <Dropdown.Item
+                      href="#/action-1"
+                      active
+                      onClick={(e) => this.changeValue(e.target.textContent)}
+                    >
+                      1
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">2</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">3</Dropdown.Item>
+                  </DropdownButton>
+                </Row>
+                <Row className={`${styles.BORow} ${styles.BtnCart}`}>
+                  <Button size="lg">Add to Cart</Button>
+                </Row>
+                <Row className={`${styles.BORow} ${styles.BtnBuy}`}>
+                  <Button variant="secondary" size="lg">
+                    Buy Now
+                  </Button>
+                </Row>
+                <Row className={`${styles.BORow} ${styles.BtnWish}`}>
+                  <Button variant="secondary" size="lg">
+                    Add to Wishlist
+                  </Button>
+                </Row>
               </Card.Body>
             </Container>
           </Col>
